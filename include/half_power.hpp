fc663f5e6ed03ad9370906e01b0c7a566f0c883d
KingsBeCattz <jdanielxd.3@gmail.com>
2026-05-01 10:28:44 -0700

    refactor: update pinout references in setHalfPower function

--- include/half_power.hpp
+++ include/half_power.hpp
@@ -7,19 +7,18 @@ extern bool half_power;
 
 void setHalfPower()
 {
-  pinMode(SPEED_LIMIT_PIN, INPUT_PULLUP);
+  pinMode(Pinout::SPEED_LIMIT_PIN, INPUT_PULLUP);
 
-  if (digitalRead(SPEED_LIMIT_PIN) == HIGH)
+  if (digitalRead(Pinout::SPEED_LIMIT_PIN) == HIGH)
   {
     half_power = true;
     Serial.println("Half power mode activated due to SPEED_LIMIT_PIN state.");
 
-    // 3 parpadeos rápidos → half power activo
     for (int i = 0; i < 3; i++)
     {
-      digitalWrite(LED_STATUS_PIN, HIGH);
+      digitalWrite(Pinout::LED_STATUS_PIN, HIGH);
       delay(100);
-      digitalWrite(LED_STATUS_PIN, LOW);
+      digitalWrite(Pinout::LED_STATUS_PIN, LOW);
       delay(100);
     }
   }
