638bd777bfa6139b88e84e40016253c4438a7eae
KingsBeCattz <jdanielxd.3@gmail.com>
2026-05-10 18:14:26 -0700

    fix: update serial logging to use SerialTools in configure function

--- include/tools/motors.hpp
+++ include/tools/motors.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <MotorDriveUnit.h>
+#include <tools/serial.hpp>
 #include <types.h>
 #include <pinout.hpp>
 #include <input_modes.hpp>
@@ -13,9 +14,9 @@ namespace MotorsTools
   inline void configure(MotorDriveUnit &driver, UnsignedPWM deadzone, const HBridgeConfig &cfg)
   {
 #ifdef DEBUG
-    Serial.printf("Configuring driver [%s] with pins: LF=%d, LB=%d, RF=%d, RB=%d, LE=%d, RE=%d, STBY=%d\n",
-                  cfg.name, cfg.left_forward, cfg.left_backward, cfg.right_forward, cfg.right_backward,
-                  cfg.left_enable, cfg.right_enable, cfg.standby);
+    SerialTools::printf("Configuring driver [%s] with pins: LF=%d, LB=%d, RF=%d, RB=%d, LE=%d, RE=%d, STBY=%d\n",
+                        cfg.name, cfg.left_forward, cfg.left_backward, cfg.right_forward, cfg.right_backward,
+                        cfg.left_enable, cfg.right_enable, cfg.standby);
 #endif
     driver.getLeftMotor().setDirectionPins(cfg.left_forward, cfg.left_backward, cfg.use_digital_directions);
     driver.getRightMotor().setDirectionPins(cfg.right_forward, cfg.right_backward, cfg.use_digital_directions);
